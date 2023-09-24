1b473c749407de8f959941bbbab2ef367a3c10c5
SanjidaMitu123 <sanjidamitu456@gmail.com>
2023-09-24 22:52:16 +0600

    router added

---
+++ src/Router/Router.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainPage from "../MainPage/MainPage";
+import Home from "../Pages/Home/Home";
+import Donation from "../Pages/Home/Donation/Donation";
+import Statistics from "../Pages/Home/Statistics/Statistics";
+
+
+const myRoute = createBrowserRouter([{
+
+
+    path : "/",
+    element : <MainPage> </MainPage>,
+    children : [
+        {
+            path : "/",
+            element : <Home> </Home> 
+        },
+        {
+            path : "/donation",
+            element : <Donation> </Donation>
+        }
+        ,
+        {
+            path : "/statistics",
+            element : <Statistics></Statistics>
+        }
+    ]
+
+}]) 
+
+export default myRoute;
---
+++ src/assets/Components/Header/Navbar.jsx
@@ -0,0 +1,50 @@
+import { NavLink } from "react-router-dom";
+import Logo from "./logo";
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <Logo></Logo>
+                <ul className=" flex gap-6">
+                    <li>
+                      
+                      
+
+                        <NavLink
+                         to="/"
+                         className={({ isActive, isPending }) =>
+                         isPending ? "pending" : isActive ? "text-[#FF444A] underline" : ""
+                          }
+                         >
+                          Home
+                       </NavLink>
+                       </li>
+                       <li>
+                       <NavLink
+                         to="/donation"
+                         className={({ isActive, isPending }) =>
+                         isPending ? "pending" : isActive ? "text-[#FF444A] underline" : ""
+                          }
+                         >
+                          Donation
+                       </NavLink>
+                    </li>
+                    <li>
+                       <NavLink
+                         to="/statistics"
+                         className={({ isActive, isPending }) =>
+                         isPending ? "pending" : isActive ? "text-[#FF444A] underline" : ""
+                          }
+                         >
+                          Statistics
+                       </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
