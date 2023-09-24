023f66b803fc28ddfeb2dd951468e5afb59cc1af
SanjidaMitu123 <sanjidamitu456@gmail.com>
2023-09-24 21:32:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1 className='p-5'>
+        <img src="/public/Logo.png" alt="" />
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
