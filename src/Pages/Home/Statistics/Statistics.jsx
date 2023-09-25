3b57581ada9cedca291e2711eaf376c6bf00c3b0
SanjidaMitu123 <sanjidamitu456@gmail.com>
2023-09-26 00:56:10 +0600

    test pie chart added

--- src/Pages/Home/Statistics/Statistics.jsx
+++ src/Pages/Home/Statistics/Statistics.jsx
@@ -1,9 +1,28 @@
+import { Pie, PieChart } from "recharts";
 
 
 const Statistics = () => {
+ 
+
+const data01 = [
+  {
+    "name": "MY DONATION",
+    "value": 7000
+  }
+];
+const data02 = [
+  {
+    "name": "TOTAL DONATION",
+    "value": 14000
+  }
+];
     return (
         <div>
-            <h1>Statistics</h1>
+            <PieChart width={730} height={250} >
+            <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#FF444A" />
+  <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#00C49F" label />
+
+            </PieChart>
         </div>
     );
 };
